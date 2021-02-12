4b565b04698798b2fc255049662e8d260316067c
Jason Hummel <jason.hummel@duda.co>
2021-02-12 10:12:37 -0700

    Adds initial api gateway routing and sample lambda
    * Include routing and lambda in infrastructure
    * Creates new lambda folder for code
    * Intial getSites lambda function.

---
+++ lambda/getSites/index.ts
@@ -0,0 +1,48 @@
+const fetch = require('node-fetch')
+
+const { API_BASE: dudaAPI, API_USER, API_PASS } = process.env;
+const buffer = Buffer.from(`${API_USER}:${API_PASS}`);
+const API_AUTH = buffer.toString('base64');
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': `Basic ${API_AUTH}`
+};
+
+exports.handler = async function (event: AWSLambda.APIGatewayEvent) {
+  const response = {
+    body: '',
+    statusCode: 400,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  try {
+    const sites = await getSites();
+    const json  = await sites.json();
+
+    response.statusCode = 200;
+    response.body = JSON.stringify(json);
+
+  } catch (e) {
+    response.body = JSON.stringify({
+      'error': `Problem handling ${event.httpMethod} on resource ${event.resource}`,
+      'description': e
+    });
+  }
+
+  return response;
+
+};
+
+const getSites = async function() {
+  const url = `${API_BASE}/sites/multiscreen/created?from=1900-01-01&to=9999-12-31`
+
+  const options = {
+    method: 'GET',
+    headers:
+  }
+
+  return fetch(url, options)
+}
--- lib/duda-instant-site-stack.ts
+++ lib/duda-instant-site-stack.ts
@@ -1,9 +1,34 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apiGateway from '@aws-cdk/aws-apigateway'
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export class DudaInstantSiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { API_USER = '', API_PASS = '', API_BASE = '' } = process.env;
+    const getSites = new lambda.Function(this, 'GetSitesLambda', {
+      code: lambda.Code.fromAsset('lambda/getSites'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      environment: {
+        API_USER,
+        API_PASS,
+        API_BASE
+      }
+    });
+
+    const api = new apiGateway.LambdaRestApi(this, 'duda', {
+      handler: getSites,
+      proxy: false
+    });
+
+    api.root.addMethod('ANY');
+
+    const sites = api.root.addResource('sites');
+    sites.addMethod('GET', new apiGateway.LambdaIntegration(getSites));
   }
 }
