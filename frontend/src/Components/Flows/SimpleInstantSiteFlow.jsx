d37ca9c825210e3b178318a362979e477d301ce6
Ted Roddy <Ted@Dudas-MacBook-Pro.local>
2021-03-11 13:38:13 -0700

    UUpdate the simple form and add material image loading

--- frontend/src/Components/Flows/SimpleInstantSiteFlow.jsx
+++ frontend/src/Components/Flows/SimpleInstantSiteFlow.jsx
@@ -7,7 +7,8 @@ const { FormControl,
         Select, 
         MenuItem,
         LinearProgress,
-        Grid
+        Grid,
+        Container
 } = require('@material-ui/core/')
 const { makeStyles, ThemeProvider } = require('@material-ui/core/styles') 
 
@@ -28,51 +29,59 @@ function SimpleInstantSiteFlow() {
   //     console.log(`Error creating site!: ${error.message}`)
   //   })
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <FormControl id="instantSite" action="#">
-        
-          <Typography variant='h4'>Simple Instant Site</Typography>
-          <Typography variant='p'>Enter your business information below to create a site:</Typography>
-          
-          <Select value={""} onChange={''} displayEmpty>
-            <MenuItem value="" name="templateId">
-              <em>Pick a template:</em>
-            </MenuItem>
-            <MenuItem value={1013306}>Creative Portfolio</MenuItem>
-            <MenuItem value={1008302}>Ice Cream Shop</MenuItem>
-            <MenuItem value={1005052}>Soap & Suds</MenuItem>
-            <MenuItem value={1002785}>My Bicycle Blog</MenuItem>
-          </Select>
+        <Container>
+          <FormControl id="instantSite" action="#">
+            <Grid container>
 
-          <Typography variant='h6'>Business Details</Typography>
-          <TextField variant="filled" name="business_name" label="Business Name"/>
-          <TextField variant="filled" multiline rowsMax="4" rows="4" name="overview" label="Overview"/>
-          <TextField variant="filled" multiline rowsMax="4" rows="4" name="aboutUs" label="About Us"/>
-          <TextField variant="filled" multiline rowsMax="4" rows="4" name="services" label="Services"/>
+              <Grid item xs={8}>
+                <Typography variant='h4'>Simple Instant Site</Typography>
+                <Typography variant='p'>Enter your business information below to create a site:</Typography>
+              </Grid>
+              
+              <Grid item xs={4}>
+                <LinearProgress variant="determinate" value={50} />
+                <Button onClick={''} variant="contained" color="primary">Create Site</Button>
+              </Grid>
 
-          <Typography variant='h6'>Address</Typography>
-          <TextField variant="filled" name="streetAddress" label="Street"/>
-          <TextField variant="filled" name="city" label="City"/>
-          <TextField variant="filled" name="region" label="Region"/>
-          <TextField variant="filled" name="country" label="Country"/>
-          <TextField variant="filled" name="postalCode" label="Postal Code"/>
+              <Grid item xs={12}>
+                <Select value={""} onChange={''} displayEmpty>
+                  <MenuItem value="" name="templateId">
+                    <em>Pick a template:</em>
+                  </MenuItem>
+                  <MenuItem value={1013306}>Creative Portfolio</MenuItem>
+                  <MenuItem value={1008302}>Ice Cream Shop</MenuItem>
+                  <MenuItem value={1005052}>Soap & Suds</MenuItem>
+                  <MenuItem value={1002785}>My Bicycle Blog</MenuItem>
+                </Select>
+              </Grid>
 
-          <Typography variant='h6'>Contact Information</Typography>
-          <TextField variant="filled" name="phoneNumber" label="Phone Number" placeholder="(800) 867-5309"/>
-          <TextField variant="filled" name="email" label="Email" placeholder="sales@company.site"/>
+              <Grid item xs={6}>
+                <Typography variant='h6'>Business Details</Typography>
+                <TextField variant="filled" name="business_name" label="Business Name" style={{ width: '100%' }}/>
+                <TextField variant="filled" multiline rowsMax="4" rows="4" name="overview" label="Overview" style={{ width: '100%' }}/>
+                <TextField variant="filled" multiline rowsMax="4" rows="4" name="aboutUs" label="About Us" style={{ width: '100%' }}/>
+                <TextField variant="filled" multiline rowsMax="4" rows="4" name="services" label="Services" style={{ width: '100%' }}/>
+                <Typography variant='h6'>Contact Information</Typography>
+                <TextField variant="filled" name="phoneNumber" label="Phone Number" placeholder="(800) 867-5309" style={{ width: '100%' }}/>
+                <TextField variant="filled" name="email" label="Email" placeholder="sales@company.site" style={{ width: '100%' }}/>
+              </Grid>
 
-          <Typography variant='h6'>Content</Typography>
-          <TextField variant="filled" name="businessImage" label="Background Image URL" placeholder="https://files.host/background.jpg"/>
-          <TextField variant="filled" name="logoUrl" label="Logo Image URL" placeholder="https://files.host/files/logo.png"/>
-          <TextField variant="filled" multiline rowsMax="4" rows='4' name="business_description" label="Description"/>
+              <Grid item xs={6}>
+                <Typography variant='h6'>Address</Typography>
+                <TextField variant="filled" name="streetAddress" label="Street" style={{ width: '100%' }}/>
+                <TextField variant="filled" name="city" label="City" style={{ width: '100%' }}/>
+                <TextField variant="filled" name="region" label="Region" style={{ width: '100%' }}/>
+                <TextField variant="filled" name="country" label="Country" style={{ width: '100%' }}/>
+                <TextField variant="filled" name="postalCode" label="Postal Code" style={{ width: '100%' }}/>
+                <Typography variant='h6'>Content</Typography>
+                <TextField variant="filled" name="businessImage" label="Background Image URL" placeholder="https://files.host/background.jpg" style={{ width: '100%' }}/>
+                <TextField variant="filled" name="logoUrl" label="Logo Image URL" placeholder="https://files.host/files/logo.png" style={{ width: '100%' }}/>
+                <TextField variant="filled" multiline rowsMax="4" rows='4' name="business_description" label="Description" style={{ width: '100%' }}/>
+              </Grid>
 
-          <LinearProgress variant="determinate" value={50} />
-          <Button onClick={''} variant="contained" color="primary">Create Site</Button>
-
-        </FormControl>
-      </Grid>
-    </Grid>
+            </Grid>
+          </FormControl>
+        </Container>
   );
 }
 
