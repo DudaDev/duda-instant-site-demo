1bf44b1b91829f241c12efb78f124a832036a525
Ted Roddy <Ted@Dudas-MacBook-Pro.local>
2021-03-11 13:38:13 -0700

    UUpdate the simple form and add material image loading

--- frontend/src/Components/Grid/FlowSelectionGridItem.jsx
+++ frontend/src/Components/Grid/FlowSelectionGridItem.jsx
@@ -1,11 +1,12 @@
 import { Typography } from "@material-ui/core";
 import { Link, withRouter } from "react-router-dom";
+import Image from 'material-ui-image';
 
 function FlowSelectionGridItem() {
     return (
         <>
             <Link to='/simple'>
-                <img src='/simplePreview.png' alt='Preview of Simple Flow' style={{width: '100%'}}/>
+                <Image src='/simplePreview.png' alt='Preview of Simple Flow' aspectRatio={1.25}/>
                 <Typography variant="h6">Simple Instant Site</Typography>
             </Link>
         </>
