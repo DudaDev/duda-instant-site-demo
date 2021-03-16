ede69b1f94eaa95e3a7a2c3a64ca48797dcdf553
Theodore Roddy <me@tedroddy.net>
2021-03-16 14:23:54 -0600

    MVP

--- frontend/src/Components/Flows/SimpleInstantSite/SimpleInstantSiteFlow.jsx
+++ frontend/src/Components/Flows/SimpleInstantSite/SimpleInstantSiteFlow.jsx
@@ -15,6 +15,9 @@ const { FormControl,
 } = require('@material-ui/core/')
 
 function SimpleInstantSiteFlow() {
+
+  const [userId, setUserId] = useState('')
+  const [siteName, setSiteName] = useState('')
   
   const [siteCreated, setSiteCreated] = useState(false)
   const [formCompleted, setFormCompleted] = useState(false)
@@ -72,7 +75,7 @@ function SimpleInstantSiteFlow() {
 
     setUpdating(true)
     setStatus(`Creating a site using template: ${form.templateId}`)
-    setProgress(0)
+    setProgress(10)
 
     const contentLibrary = {
         "location_data":{
@@ -120,50 +123,51 @@ function SimpleInstantSiteFlow() {
 
     Duda.createSite(form.templateId)
       .then(response => {
-        const siteName = response.siteName
+        const site = response.siteName
+        setSiteName(site)
+        console.log(response)
         setUpdating(true)
-        setStatus(`Site created: ${siteName}`)
-        setProgress(20)
-        Duda.uploadContent(siteName, contentLibrary)
+        setStatus(`Site created: ${site}`)
+        setProgress(30)
+        Duda.updateContent(site, contentLibrary)
           .then(response => {
+            console.log(response)
             setStatus(response.status)
-            setProgress(40)
+            setProgress(50)
             Duda.createUser(userId)
               .then(response => {
-                setStatus(response.status)
-                setProgress(60)
-                Duda.grantUserAccess(userId, siteName)
+                console.log(response)
+                setUserId(response.userId)
+                setStatus(`User created in Duda: ${response.userId}`)
+                setProgress(80)
+                Duda.grantUserAccess(response.userId, site)
                   .then(response => {
+                    console.log(response)
                     setStatus(response.status)
-                    setProgress(80)
-                    Duda.grantUserAccess(userId, siteName)
-                      .then(response => {
-                        setStatus(response.status)
-                        setProgress(100)
-                        setSiteCreated(true)
-                        setUpdating(false)
-                      })
-                      .catch(error => {
-                        setErrored(true)
-                        setStatus(error)
-                      })
+                    setProgress(100)
+                    setSiteCreated(true)
+                    setUpdating(false)
                   })
                   .catch(error => {
+                    console.log(error)
                     setErrored(true)
                     setStatus(error)
                   })
               })
               .catch(error => {
+                console.log(error)
                 setErrored(true)
                 setStatus(error)
               })
           })
           .catch(error => {
+            console.log(error)
             setErrored(true)
             setStatus(error)
           })
       })
       .catch(error => {
+        console.log(error)
         setErrored(true)
         setStatus(error)
       })
@@ -181,8 +185,8 @@ function SimpleInstantSiteFlow() {
       "region": 'California',
       "city": 'San Francisco',
       "country": 'US',
-      "businessDescription": "Duda is the leading web design platform for companies that offer web design services to small businesses. We serve all types of customers, from freelance web professionals and digital agencies, all the way up to the largest hosting companies and online publishers in the world.",
-      "logoUrl": 'ttps://irt-cdn.multiscreensite.com/7536fe2010ed4f7ea68e21d0cb868e01/dms3rep/multi/ice_cream_logo_b_w-18-300x300.svg',
+      "businessDescription": "We serve all types of customers, from freelance web professionals and digital agencies, all the way up to the largest hosting companies and online publishers in the world.",
+      "logoUrl": 'https://irt-cdn.multiscreensite.com/7536fe2010ed4f7ea68e21d0cb868e01/dms3rep/multi/ice_cream_logo_b_w-18-300x300.svg',
       "overview": 'Oh, Duda? Duda is a variation of "Dude", who just happens to be the main character in one of our favorite movies of all time: The Big Lebowski. You should watch it some time. Look out for that ferret!',
       "services": 'Responsive Website Builder',
       "aboutUs": 'Duda is a leading website builder for web professionals and agencies of all sizes. Our website builder enables you to build amazing, feature-rich websites that are perfectly suited to desktop, tablet and mobile. Our mobile builder enables you to build mobile-only sites from scratch, or based on an existing desktop site or Facebook business page. Duda allows professionals and agencies to build high-converting, personalized websites at scale. Duda optimizes each and every site for Google PageSpeed.',
@@ -216,7 +220,7 @@ function SimpleInstantSiteFlow() {
               </Grid>
 
               <Grid item xs={2} style={{ marginTop: '30px' }}>
-                <SimpleTemplateSelector handleChange={handleChange} form={form}/>
+                <SimpleTemplateSelector handleChange={handleChange} form={form} updating={updating}/>
               </Grid>
               
               <Grid item xs={2} style={{ marginTop: '30px' }}>
@@ -225,11 +229,14 @@ function SimpleInstantSiteFlow() {
                     variant="contained" 
                     color="default" 
                     style={{ marginLeft: '20px' }}
+                    disabled={updating}
                     >Auto-fill</Button>
               </Grid>
 
               <Grid item xs={8}>
                 <SimpleSiteControls 
+                    siteName={siteName}
+                    userId={userId}
                     updating={updating} 
                     siteCreated={siteCreated} 
                     formCompleted={formCompleted}
@@ -243,8 +250,8 @@ function SimpleInstantSiteFlow() {
                 <TextField onChange={handleChange} variant="filled" value={form.aboutUs} disabled={updating} multiline rowsMax="4" rows="4" name="aboutUs" label="About Us" style={{ width: '100%' }}/>
                 <TextField onChange={handleChange} variant="filled" value={form.services} disabled={updating} multiline rowsMax="4" rows="4" name="services" label="Services" style={{ width: '100%' }}/>
                 <Typography variant='h6' style={{ marginTop: '30px', marginBottom: '10px' }}>Contact Information</Typography>
-                <TextField onChange={handleChange} variant="filled" value={form.phoneNumber} disabled={updating} name="phoneNumber" label="Phone Number" placeholder="(800) 867-5309" style={{ width: '100%' }}/>
-                <TextField onChange={handleChange} variant="filled" value={form.email} disabled={updating} name="email" label="Email" placeholder="sales@company.site" style={{ width: '100%' }}/>
+                <TextField onChange={handleChange} variant="filled" value={form.phoneNumber} disabled={updating} name="phoneNumber" label="Phone Number" placeholder="8008675309" style={{ width: '100%' }}/>
+                <TextField onChange={handleChange} variant="filled" key={Math.random()} disabled={updating} name="email" label="Email" placeholder="sales@company.site" style={{ width: '100%' }} value={form.email}/>
               </Grid>
 
               <Grid item xs={6}>
@@ -257,7 +264,7 @@ function SimpleInstantSiteFlow() {
                 <Typography variant='h6' style={{ marginTop: '30px', marginBottom: '10px' }}>Content</Typography>
                 <TextField onChange={handleChange} variant="filled" value={form.backgroundUrl} disabled={updating} name="backgroundUrl" label="Background Image URL" placeholder="https://files.host/background.jpg" style={{ width: '100%' }}/>
                 <TextField onChange={handleChange} variant="filled" value={form.logoUrl} disabled={updating} name="logoUrl" label="Logo Image URL" placeholder="https://files.host/files/logo.png" style={{ width: '100%' }}/>
-                <TextField onChange={handleChange} variant="filled" value={form.businessDescription} disabled={updating} multiline rowsMax="4" rows='4' name="businessDescription" label="Description" style={{ width: '100%' }}/>
+                <TextField onChange={handleChange} variant="filled" key={Math.random()} disabled={updating} multiline rowsMax="4" rows='4' name="businessDescription" label="Description" style={{ width: '100%' }} value={form.businessDescription}/>
               </Grid>
 
             </Grid>
